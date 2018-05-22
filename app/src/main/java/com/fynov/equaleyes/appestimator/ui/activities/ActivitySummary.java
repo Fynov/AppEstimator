bda065db065ab7a545c56e0797ddb5e29bc064af
Tadej16 <37588368+Tadej16@users.noreply.github.com>
2018-05-22 19:39:31 +0200

    Added the last screen, only need to set up the button so it shows the last page

---
+++ app/src/main/java/com/fynov/equaleyes/appestimator/ui/activities/ActivitySummary.java
@@ -0,0 +1,15 @@
+package com.fynov.equaleyes.appestimator.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fynov.equaleyes.appestimator.R;
+
+public class ActivitySummary extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_summary);
+    }
+}
